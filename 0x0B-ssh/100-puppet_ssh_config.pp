0dc805d045a917de390c5f3bcacf548f6c552143
Juan Duque <84897255+dkb4rb@users.noreply.github.com>
2022-01-06 04:00:46 +0000

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,1 +1,13 @@
-#!/usr/bin/env bash
+# Configure ssh config file
+
+file_line { 'Turn off passwd auth':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true
+}
+
+file_line { 'Declare identity file':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/holberton',
+  replace => true
+}
