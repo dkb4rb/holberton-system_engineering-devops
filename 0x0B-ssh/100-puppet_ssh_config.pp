d2634866d16e862e2122f0b7a2a115419a2cac72
Juan Duque <84897255+dkb4rb@users.noreply.github.com>
2022-01-06 04:01:18 +0000

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -8,6 +8,6 @@ file_line { 'Turn off passwd auth':
 
 file_line { 'Declare identity file':
   path    => '/etc/ssh/ssh_config',
-  line    => '    IdentityFile ~/.ssh/holberton',
+  line    => '    IdentityFile ~/.ssh/school',
   replace => true
 }
